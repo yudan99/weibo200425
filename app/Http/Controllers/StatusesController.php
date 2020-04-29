07938ecd4647c4ec731f8a813b50ef08ff5e1988
YaoXin <2222122099@qq.com>
2020-04-29 13:25:49 +0000

    发布微博

---
+++ app/Http/Controllers/StatusesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Status;
+
+class StatusesController extends Controller
+{
+    //接下来我们需要开发的 store 和 destroy 动作将用于对微博的创建和删除，这两个动作都需要用户登录，因此让我们借助 Auth 中间件来为这两个动作添加过滤请求。
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:140'
+        ]);
+
+        Auth::user()->statuses()->create([
+            'content' => $request['content']
+        ]);
+        session()->flash('success', '发布成功！');
+        return redirect()->back();
+    }
+}
--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+    protected $fillable = ['content'];
     //1-1
     public function user()
     {
