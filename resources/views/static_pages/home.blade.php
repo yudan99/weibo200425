87cc9f1bc0e4469bf0000a98815bbf60eb26c381
YaoXin <2222122099@qq.com>
2020-04-29 13:46:18 +0000

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @if (Auth::check())
-    <div class="row">
+<!--     <div class="row">
       <div class="col-md-8">
         <section class="status_form">
           @include('shared._status_form')
@@ -13,7 +13,24 @@
           @include('shared._user_info', ['user' => Auth::user()])
         </section>
       </aside>
+    </div> -->
+
+    <div class="row">
+      <div class="col-md-8">
+        <section class="status_form">
+          @include('shared._status_form')
+        </section>
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
+      </div>
+      <aside class="col-md-4">
+        <section class="user_info">
+          @include('shared._user_info', ['user' => Auth::user()])
+        </section>
+      </aside>
     </div>
+
   @else
     <div class="jumbotron">
       <h1>Hello Laravel</h1>
